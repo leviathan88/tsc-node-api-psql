9dcbc6edb827a53d34cae1bf7f7a08b7fbda868c
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 17:34:51 +0100

    adding routes

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,7 +1,10 @@
+import { apiCreateLesson } from './apiCreateLesson'
+import { apiGetCourseDetails } from './apiGetCourseDetails'
 import { Application } from 'express'
-
 import { apiGetAllCourses } from './apiGetAllCourses'
 
 export const initRestApi=(app: Application) => {
     app.route('/api/courses').get(apiGetAllCourses)
+    app.route('/api/courses/:id').get(apiGetCourseDetails)
+    api.route('/api/lesson').post(apiCreateLesson)
 }
