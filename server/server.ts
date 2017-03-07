6a4bb44699e7b4fb6c129cfe239a540a145fc539
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 18:58:30 +0100

    using bodyPArser

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,10 @@ import { apiErrorHandler } from './api/apiErrorHandler'
 import { initRestApi } from './api/api'
 import * as express from 'express'
 
-const app = express()
+const bodyParser = require('body-parser')
 
+const app = express()
+app.use(bodyParser.json())
 app.set('port', 4000)
 
 initRestApi(app)
