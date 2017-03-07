a51eec2a11b772f21951397d1d65b9ecf39911ac
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 18:58:39 +0100

    adding delete route

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,10 +1,13 @@
+import { apiPatchLesson } from './apiPatchLesson'
 import { apiCreateLesson } from './apiCreateLesson'
 import { apiGetCourseDetails } from './apiGetCourseDetails'
 import { Application } from 'express'
 import { apiGetAllCourses } from './apiGetAllCourses'
+import { apiDeleteLesson } from './apiDeleteLesson'
 
 export const initRestApi=(app: Application) => {
     app.route('/api/courses').get(apiGetAllCourses)
     app.route('/api/courses/:id').get(apiGetCourseDetails)
     app.route('/api/lesson').post(apiCreateLesson)
+    app.route('/api/lesson/:id').patch(apiPatchLesson).delete(apiDeleteLesson)
 }
