b15cee95e892bc150fc451cf7f7baafabbd43329
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 17:35:40 +0100

    finishing findAllCOurses api

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -1,9 +1,11 @@
+import { onSuccess } from './onSuccess'
+import { onError } from './onError'
 import { Request, Response } from 'express'
-
+import * as _ from 'lodash'
 import { findAllCourses } from '../queries/findAllCourses'
 
 export const apiGetAllCourses = (req: Request, res: Response) => {
-    findAllCourses().then(results=>
-        res.status(200).json({results})
-    )
+    findAllCourses()
+        .then(_.partial(onSuccess, res))
+        .catch(_.partial(onError, res, "Find all courses failed"))
 }
