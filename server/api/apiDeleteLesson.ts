274c54f0118a1c090f5f8a8ec5e991265245da05
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 18:59:18 +0100

    creating apiDeleteLEsson

---
+++ server/api/apiDeleteLesson.ts
@@ -0,0 +1,13 @@
+import { onError } from './onError'
+import { databaseErrorHandler } from './databaseErrorHandler'
+import { onSuccess } from './onSuccess'
+import { Request, Response } from 'express'
+import { deleteLesson } from '../queries/deleteLesson'
+
+export const apiDeleteLesson = (req: Request, res: Response) => {
+    const lessonId = req.params.id
+    deleteLesson(lessonId)
+        .then(_.partial(onSuccess, res))
+        .catch(_.partial(databaseErrorHandler, res))
+        .catch(_.partial(onError, res, "Could not update lesson"))
+}
