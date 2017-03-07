586163079a3657ce910b76a6014825e99fa403db
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 17:34:43 +0100

    including error handler

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
-import { initRestApi } from './api/api';
+import { apiErrorHandler } from './api/apiErrorHandler'
+import { initRestApi } from './api/api'
 import * as express from 'express'
 
 const app = express()
 
 app.set('port', 4000)
+
 initRestApi(app)
+app.use(apiErrorHandler)
+
 app.listen(app.get('port'), ()=>console.log(`server is runnng at ${app.get('port')}`))
