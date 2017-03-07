1372f6d7af306c6260edd13138c156c8c606924f
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 17:36:27 +0100

    adding new stuff to the model

--- server/models/model.ts
+++ server/models/model.ts
@@ -1,6 +1,6 @@
+import { initLessonModel } from './initLessonModel'
 import * as ORM from 'sequelize'
 import { Sequelize, LoggingOptions } from 'sequelize'
-
 import Config from '../../config'
 import { initCourseModel } from './../models/initCourseModel'
 
@@ -9,3 +9,7 @@ const options: LoggingOptions = {benchmark: true, logging: console.log}
 const sequelize: Sequelize = new ORM(Config.dbUrl, options)
 
 export const CourseModel = initCourseModel(sequelize)
+export const LessonModel = initLessonModel(sequelize)
+
+CourseModel.hasMany(LessonModel, {foreignKey: 'courseId'})
+LessonModel.belongsTo(CourseModel, {foreignKey: 'courseId'})
