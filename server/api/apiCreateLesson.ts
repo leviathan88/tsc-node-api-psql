8dd14d73d1cdb0c88ad590e6567e04cdfec13393
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 19:16:47 +0100

    cleaning up

--- server/api/apiCreateLesson.ts
+++ server/api/apiCreateLesson.ts
@@ -5,14 +5,10 @@ import { onSuccess } from './onSuccess'
 import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler } from 'express'
 import * as _ from 'lodash'
 
-
-
 export const apiCreateLesson=(err: any, req: Request, res: Response, next: NextFunction)=> {
 
     createLesson(req.body)
         .then(_.partial(onSuccess, res))
         .catch(_.partial(databaseErrorHandler, res))
         .catch(_.partial(onError, res, "Could not create lesson"))
-    
-
 }
