f94d841b7b88c1fae35aaabb6656fc64b3407cb5
Elvis Dedic <elvis26112009@gmail.com>
2017-03-07 18:59:09 +0100

    creating apiCreateLessons

---
+++ server/api/apiCreateLesson.ts
@@ -0,0 +1,18 @@
+import { databaseErrorHandler } from './databaseErrorHandler'
+import { createLesson } from './../queries/createLesson'
+import { onError } from './onError'
+import { onSuccess } from './onSuccess'
+import { Request, Response, NextFunction, RequestHandler, ErrorRequestHandler } from 'express'
+import * as _ from 'lodash'
+
+
+
+export const apiCreateLesson=(err: any, req: Request, res: Response, next: NextFunction)=> {
+
+    createLesson(req.body)
+        .then(_.partial(onSuccess, res))
+        .catch(_.partial(databaseErrorHandler, res))
+        .catch(_.partial(onError, res, "Could not create lesson"))
+    
+
+}
